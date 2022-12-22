dd63723fb3c40433b0209ea929da88b154b174fe
Zaid <zaidansari42@gmail.com>
2022-12-22 16:36:16 +0530

    Completed the Game

--- app.js
+++ app.js
@@ -13,20 +13,94 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-diceEl.classList.add('hidden');
+let scores, playing, currentScore, currentPlayer;
 
-let scores = [0, 0];
+const initialize = function () {
+  diceEl.classList.add('hidden');
 
-[score0El.textContent, score1El.textContent] = scores;
+  scores = [0, 0];
+
+  [score0El.textContent, score1El.textContent] = scores;
+
+  playing = true;
+
+  currentPlayer = 0;
+
+  currentScore = 0;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  document.getElementById('name--0').textContent = 'PLAYER 1';
+  document.getElementById('name--1').textContent = 'PLAYER 2';
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player1El.classList.remove('player--active');
+  player0El.classList.add('player--active');
+};
+
+initialize();
 
 const rollDice = function () {
-  // Making the hidden dice visible
-  diceEl.classList.remove('hidden');
+  if (playing) {
+    // Making the hidden dice visible
+    diceEl.classList.remove('hidden');
 
-  //   Generating random dice between 1 and 6
-  const dice = Math.trunc(Math.random() * 6 + 1);
+    //   Generating random dice between 1 and 6
+    const dice = Math.trunc(Math.random() * 6 + 1);
 
-  diceEl.setAttribute('src', `images/dice-${dice}.png`);
+    diceEl.setAttribute('src', `images/dice-${dice}.png`);
+
+    if (dice !== 1) {
+      currentScore += dice;
+
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+};
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${currentPlayer}`).textContent =
+    currentScore;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+};
+
+const holdDice = function () {
+  scores[`${currentPlayer}`] += currentScore;
+
+  document.getElementById(`score--${currentPlayer}`).textContent =
+    scores[`${currentPlayer}`];
+
+  currentScore = 0;
+  current0El.textContent = 0;
+
+  if (scores[`${currentPlayer}`] >= 10) {
+    document
+      .querySelector(`.player--${currentPlayer}`)
+      .classList.add('player--winner');
+
+    document.getElementById(`name--${currentPlayer}`).textContent = `Player ${
+      currentPlayer + 1
+    } WINS!`;
+
+    playing = false;
+
+    diceEl.classList.add('hidden');
+  } else {
+    switchPlayer();
+  }
 };
 
 btnRoll.addEventListener('click', rollDice);
+btnHold.addEventListener('click', holdDice);
+btnNew.addEventListener('click', initialize);
